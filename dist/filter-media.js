f13fc2f09cfc6205c72728e3f7578dd70966925a
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2025-06-21 11:15:20 +0200

    Update filter-media.js

--- dist/filter-media.js
+++ dist/filter-media.js
@@ -2,13 +2,17 @@
 exports.__esModule = true;
 /** @internal */
 var filterMedia = function (media, predicateObj) {
+    // Dodane: filtruj, aby nie wybierać snipped (czyli skróconych)
     return media.filter(function (_a) {
-        var format = _a.format;
+        var format = _a.format, snipped = _a.snipped;
         var match = false;
         if (predicateObj.protocol)
             match = format.protocol === predicateObj.protocol;
         if (predicateObj.format)
             match = format.mime_type === predicateObj.format;
+        // DODANE: wyklucz jeśli snipped
+        if (snipped)
+            match = false;
         return match;
     });
 };
