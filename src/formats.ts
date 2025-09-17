27854a6f0ccb6253ab7ffa4a880d1129eb03cf67
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2025-09-17 16:58:59 +0200

    Update formats.ts

--- src/formats.ts
+++ src/formats.ts
@@ -1,15 +1,17 @@
-/**
- * Audio formats a track can be encoded in.
- */
 enum FORMATS {
   MP3 = 'audio/mpeg',
-  OPUS = 'audio/ogg; codecs="opus"'
+  OPUS = 'audio/ogg; codecs="opus"',
+  AAC = 'audio/mp4; codecs="mp4a.40.2"',
+  FLAC = 'audio/flac',
+  WAV = 'audio/wav'
 }
 
-/** @internal */
 export const _FORMATS = {
   MP3: FORMATS.MP3,
-  OPUS: FORMATS.OPUS
-}
+  OPUS: FORMATS.OPUS,
+  AAC: FORMATS.AAC,
+  FLAC: FORMATS.FLAC,
+  WAV: FORMATS.WAV
+} as const
 
 export default FORMATS
