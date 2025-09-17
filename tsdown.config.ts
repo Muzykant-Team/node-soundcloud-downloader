92db16efe717370961cf2be3cc5a7f620d35d3b7
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2025-09-17 19:09:22 +0200

    Update tsdown.config.ts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig([
   {
-    entry: ['./src/index.ts'],
-    platform: 'neutral',
-    dts: true,
+    entry: ['./src/index.ts'],   
+    outDir: 'dist',              
+    platform: 'neutral',         
+    format: ['cjs', 'esm'],      
+    dts: true,                   
+    sourcemap: true,             
+    minify: true,                
+    clean: true                  
   },
 ])
