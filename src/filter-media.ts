bdfef94deec35f7f517cf869bf47ad3587cb146c
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2025-09-26 18:34:26 +0200

    Update filter-media.ts

--- src/filter-media.ts
+++ src/filter-media.ts
@@ -3,19 +3,29 @@ import FORMATS from './formats'
 import STREAMING_PROTOCOLS from './protocols'
 
 export interface FilterPredicateObject {
-  protocol?: STREAMING_PROTOCOLS,
+  protocol?: STREAMING_PROTOCOLS
   format?: FORMATS
 }
 
 /** @internal */
-const filterMedia = (media: Transcoding[], predicateObj: FilterPredicateObject): Transcoding[] => {
+const filterMedia = (
+  media: Transcoding[],
+  predicate: FilterPredicateObject
+): Transcoding[] => {
   return media.filter(({ format, snipped }) => {
-    let match = false
-    if (predicateObj.protocol) match = format.protocol === predicateObj.protocol
-    if (predicateObj.format) match = format.mime_type === predicateObj.format
-    // DODAJ TO:
-    if (snipped) match = false
-    return match
+    if (snipped) {
+      return false
+    }
+
+    if (predicate.protocol && format.protocol !== predicate.protocol) {
+      return false
+    }
+
+    if (predicate.format && format.mime_type !== predicate.format) {
+      return false
+    }
+
+    return true
   })
 }
 
