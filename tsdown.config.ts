0bab0a4b0efb95dc048d454fa7133e749bdc2b78
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2025-11-11 16:49:47 +0100

    Add 'cjs' format to tsdown configuration

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,7 +5,7 @@ export default defineConfig([
     entry: ['./src/index.ts'],   
     outDir: 'dist',              
     platform: 'node',         
-    format: ['esm'],      
+    format: ['esm', 'cjs'],      
     dts: true,                   
     sourcemap: true,             
     minify: true,                
